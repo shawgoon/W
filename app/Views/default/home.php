f7dacb464686981d7daa41d498dc4421f64b6e78
shawgoon <alban.havard@aliceadsl.fr>
2017-01-13 15:10:04 +0100

    signup & login

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -4,4 +4,6 @@
 	<h2>Let's code.</h2>
 	<p>Vous avez atteint la page d'accueil. Bravo.</p>
 	<p>Et maintenant, RTFM dans <strong><a href="../docs/tuto/" title="Documentation de W">docs/tuto</a></strong>.</p>
+	<p>Vous inscrire <strong><a href="../public/signup/" title="inscription">Inscription</a></strong>.</p>
+	<p>Vous connecter <strong><a href="../public/login/" title="connection">Connection</a></strong>.</p>
 <?php $this->stop('main_content') ?>
--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,12 @@
 <?php
-	
+
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'default_home'],
+		['GET', '/articles/', 'Articles#afficher', 'articles'],
+		['GET|POST', '/signup/', 'UsersSignup#afficher', 'users_signup'],
+		['GET|POST', '/validSignup/', 'UsersValidSignup#afficher', 'users_validSignup'],
+		['GET|POST', '/login/', 'UsersLogin#afficher', 'users_login'],
+		['GET|POST', '/account/', 'UsersAccount#afficher', 'users_account'],
+		['GET', '/logout/', 'UsersLogout#afficher', 'users_logout'],
+
 	);
